9994dbd6ae8912e480eb218bf0d87e695a53f3ea
Kaushalyasl <149307067+Kaushalyasl@users.noreply.github.com>
2024-12-19 21:43:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || 'X6oTURID#1Yfh0vTHiEIH_YCXVg2Z63DHd8CJ8qe89xkDgtFGnEg', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94750122621',    // Enter Your Owner Number
 };
